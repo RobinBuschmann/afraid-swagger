c814a92c87cca6150d3e52a688f5b0483f31b437
RobinBuschmann <robinbuschmann@hotmail.de>
2018-12-07 13:12:39 +0100

    Adds readme; Defines api; Adjusts package.json; Prepares for ci and publishing

---
+++ README.md
@@ -0,0 +1,83 @@
+[![NPM](https://img.shields.io/npm/v/afraid-swagger.svg)](https://www.npmjs.com/package/afraid-swagger)
+[![Build Status](https://travis-ci.org/RobinBuschmann/afraid-swagger.svg?branch=master)](https://travis-ci.org/RobinBuschmann/afraid-swagger)
+
+# afraid-swagger
+> You already described all you routes with afraid? Fear the redundancy? Let's get a swagger documentation with nearly no effort!
+
+## Installation
+```bash
+npm install afraid --save --no-optional
+npm install afraid-swagger --save
+```
+
+## Usage
+```typescript
+import {query, f, fail} from 'afraid';
+import {swagger, responseBody} from 'afraid-swagger';
+import * as express from 'express';
+
+const app = express();
+
+app.use('/api-docs', swagger({version: '1.0', title: 'API Docs'}));
+
+app.get('/users', [
+    query(
+        f('limit').int(),
+        f('offset').int(),
+        f('filters').string().array().opt(),
+    ),
+    responseBody(
+        f('id').int(),
+        f('name').string(),
+    ),
+    fail,
+], (req, res, next) => {
+    // ...
+});
+```
+
+## Using classes for validation and transformation (optional)
+
+#### Installation
+Omitting `--no-optional` will install required packages `class-transformer` and `reflect-metadata` automatically
+```
+npm install afraid --save 
+```
+#### Configuration
+The following flags in `tsconfig.json`:
+```json
+{
+  "experimentalDecorators": true,
+  "emitDecoratorMetadata": true
+}
+```
+
+#### Usage
+```typescript
+import {body, Field, IsInt, fail} from 'afraid';
+import {swagger, responseBody} from 'afraid-swagger';
+import * as express from 'express';
+
+const app = express();
+
+class CreateUserDTO {
+    @Field name: string;
+    @IsInt() @Field age: number;
+}
+
+class UserDTO {
+    @Field id: number;
+    @Field name: string;
+    @IsInt() @Field age: number;
+}
+
+app.use('/api-docs', swagger({version: '2.0', title: 'API Docs'}));
+
+app.post('/users', [
+    body(CreateUserDTO),
+    responseBody(UserDTO),
+    fail,
+], (req, res, next) => {
+    // ...
+});
+```
--- index.ts
+++ index.ts
@@ -1,75 +1,26 @@
 ///<reference path="lib/afraid-extension.d.ts"/>
+export * from './lib/common/string';
+export * from './lib/common/object';
 
-import * as express from 'express';
-import {Router} from 'express';
-import {body, f, Field, params, query} from 'afraid';
-import {swagger} from './lib/swagger';
-import {createServer} from 'http';
-import {responseBody, responseHeaders} from './lib/meta/middlewares';
+export * from './lib/express/routes';
+export * from './lib/express/types';
 
-const app = express();
-const handler = (_, res) => res.sendStatus(200);
+export * from './lib/json-schema/json-schema';
 
-class UpdateUserDTO {
-    @Field name: string;
-    @Field birthday: Date;
-}
+export * from './lib/meta/middlewares';
 
-class UserDTO {
-    @Field id: string;
-    @Field name: string;
-    @Field birthday: Date;
-}
+export * from './lib/openapi/components/components';
 
-app.use('/api-docs', swagger({
-    title: 'afraid-swagger',
-    version: '1.0',
-}));
+export * from './lib/openapi/info/info';
 
-app.get('/users', [
-    query(
-        f('limit').int().opt(),
-        f('offset').int().opt(),
-        f('filters').string().array().opt(),
-    ),
-    responseBody(UserDTO).array(),
-], handler);
-app.get('/users/:id', [params(f('id').int())], handler);
+export * from './lib/openapi/paths/content';
+export * from './lib/openapi/paths/parameter';
+export * from './lib/openapi/paths/path';
+export * from './lib/openapi/paths/request-body';
+export * from './lib/openapi/paths/response';
 
-app.post('/users', [
-        body(
-            f('name').string(),
-            f('age').int(),
-        ),
-        responseBody(
-            f('id').string(),
-            f('name').string(),
-            f('age').int(),
-        ).httpCode(200),
-        responseHeaders(
-            f('X-Auth-Header').string()
-                .description('Auth token')
-        ).httpCode(200),
-    ],
-    (req, res, next) => {
+export * from './lib/openapi/servers/server';
 
-    });
+export * from './lib/openapi/document.spec';
 
-
-app.put('/users/:id', [
-    params(f('id').int()),
-    body(UpdateUserDTO),
-], handler);
-
-const friends = Router();
-
-friends.get('/friends', handler);
-friends.post('/friends', handler);
-
-app.use(friends);
-
-app.use((err, req, res, next) => {
-   res.send(err);
-});
-
-createServer(app).listen(3000, () => console.log('server runs'));
+export * from './lib/swagger';
